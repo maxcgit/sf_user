2a87bf38eff2e847f14738841d0c95acae79ca29
Max <maxcim@gmail.com>
2014-07-02 06:54:09 -0500

    Test&APC

--- src/Maxc/SiteBundle/Tests/Controller/DefaultControllerTest.php
+++ src/Maxc/SiteBundle/Tests/Controller/DefaultControllerTest.php
@@ -10,8 +10,17 @@ class DefaultControllerTest extends WebTestCase
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertEquals('Maxc\SiteBundle\Controller\DefaultController::indexAction', $client->getRequest()->attributes->get('_controller'));
+    }
+
+    public function testAbout()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/about');
+
+        $this->assertTrue($crawler->filter('h1:contains("About")')->count() > 0);
     }
 }
