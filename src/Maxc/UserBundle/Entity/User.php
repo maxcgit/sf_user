b4dd7e2bc41da6b27fb6c86c850b5dc04bfb1359
Max <maxcim@gmail.com>
2014-10-21 03:53:57 -0500

    remove oauth2

--- src/Maxc/UserBundle/Entity/User.php
+++ src/Maxc/UserBundle/Entity/User.php
@@ -29,18 +29,6 @@ class User extends BaseUser
      */
     use TimestampableEntity;
 
-    /** @ORM\Column(name="facebook_id", type="string", length=255, nullable=true) */
-    protected $facebook_id;
-
-    /** @ORM\Column(name="facebook_access_token", type="string", length=255, nullable=true) */
-    protected $facebook_access_token;
-
-    /** @ORM\Column(name="google_id", type="string", length=255, nullable=true) */
-    protected $google_id;
-
-    /** @ORM\Column(name="google_access_token", type="string", length=255, nullable=true) */
-    protected $google_access_token;
-
     public function __construct()
     {
         parent::__construct();
